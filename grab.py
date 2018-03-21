7273a766b6322c115add03bd2199e99caee52cbd
Ripjack <theranslinkard@gmail.com>
2018-03-21 10:39:32 -0500

    logging
    Signed-off-by: Ripjack <theranslinkard@gmail.com>

--- grab.py
+++ grab.py
@@ -45,6 +45,7 @@ try:
             log.write("screenshotting enabled" + "\n")
             log.write("thumbnail percent = quality = " + str(quality) + "\n")
             subprocess.run(['scrot', '--thumb', str(thumb_perc), '-q', str(quality), '{}.jpg'.format(hostname)])
+            subprocess.run(['rm', '{}.jpg'.format(hostname)])
             with open('{}-thumb.jpg'.format(hostname), 'rb') as f:
                 log.write("Opened local img successfully" + "\n")
                 img = f.read()
