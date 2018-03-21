9ff11325161105701a8eff22b18a9e890d01369c
Ripjack <theranslinkard@gmail.com>
2018-03-21 11:05:12 -0500

    renamed time var
    Signed-off-by: Ripjack <theranslinkard@gmail.com>

--- grab.py
+++ grab.py
@@ -28,7 +28,7 @@ try:
     headers = {'Content-type': 'application/json'}
 
     while arch:
-        if time.time() - begin > 900:
+        if time.time() - begin > 300:
             log.close()
             subprocess.run(['rm', '{}-grab_log.txt'.format(logtime)])
             logtime = time.strftime("%Y-%m-%d-%H-%M-%S")
