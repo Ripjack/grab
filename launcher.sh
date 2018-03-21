f3583c9c3332715bb5b08180a1741bf777bd6ef3
Ripjack <theranslinkard@gmail.com>
2018-03-21 14:30:50 -0500

    useless now

--- launcher.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#launcher.sh
-#exec grab with python3
-
-cd /home/pi/grab/
-export DISPLAY=:0.0
-export XAUTHORITY=~/.Xauthority
-XAUTHORITY=~/.Xauthority DISPLAY=:0.0 sudo /usr/bin/python3 /home/pi/grab/grab.py
